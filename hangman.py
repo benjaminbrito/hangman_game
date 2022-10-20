315c88eba7580422a944fb3434d278436ac1bbd1
benjaminbrito <benjaminalexbrito@gmail.com>
2022-10-20 01:31:20 -0600

    se envian los cambios del juego, ya es funcional y se puede jugar

--- hangman.py
+++ hangman.py
@@ -1,8 +1,49 @@
 
+from ast import While
 import random
 
 def run():
-    random_word()
+    list_characters = []
+    word = random_word()
+    underscore = num_underscore(word)
+    list_characters = list_word(word)
+
+    print(word)
+    print(len(word))
+    print(list_characters)
+    print(' '.join(map(str, underscore)))
+
+    while hay_underscore(word, underscore):
+        user_char = input('Ingrese una letra: ')
+        coincidencia(user_char,word,underscore)
+        print(' '.join(map(str, underscore)))
+    
+    print('ganaste')
+        
+
+def coincidencia(user_char,word,underscore):
+    for i in range(len(word)):
+        if word[i] ==user_char:
+            underscore[i] = word[i]
+
+def hay_underscore (word,underscore):
+    for i in range(len(word)):
+        if underscore[i] == '_':
+            return True
+
+    return False
+
+def list_word(word):
+    list_characters = []
+    for i in range(len(word)):
+        list_characters.append(word[i]) 
+    return list_characters
+
+def num_underscore(word):
+    underscore =[]
+    for x in range(len(word)):
+        underscore = underscore  + ['_']
+    return underscore
 
 
 def random_word():
@@ -10,9 +51,8 @@ def random_word():
     with open("./files/words.txt","r",encoding='utf_8') as f:
         for line in f:
             words.append(line)
-    
-    print(random.choice(words))
-    
+    return random.choice(words).strip()
+
 
 if __name__ =='__main__':
     run()
